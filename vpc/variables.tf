5c5843ce3f4231f01d2f51e5873d4d3e2bb379d1
KhuslenS <khuslen.cyber@gmail.com>
2022-04-07 16:40:32 -0500

    variables are updated

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -24,15 +24,15 @@ variable "rt_cidr_block" {
 # Subnet variables
 variable "subnet_azs" {
   type        = list(string)
-  default = [ "us-west-1a", "us-west-1b", "us-west-1c"]
+  default = [ "us-west-1a", "us-west-1c"]
 }
 
 variable "pub_cidr_subnet" {
   type        = list(string)
-  default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default = [ "10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "priv_cidr_subnet" {
   type        = list(string)
-  default = [ "10.0.11.0/24", "10.0.12.0/24", "10.0.13.0/24"]
+  default = [ "10.0.11.0/24", "10.0.12.0/24"]
 }
