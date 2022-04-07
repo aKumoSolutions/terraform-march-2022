f327b32c61509516de1b266976cfa66dd9efe08b
KhuslenS <khuslen.cyber@gmail.com>
2022-04-06 20:03:00 -0500

    dynamodb lock is added to backend.tf

--- session-5/backend.tf
+++ session-5/backend.tf
@@ -3,7 +3,10 @@ terraform {
         bucket = "terraform-march-2022-backend-bucket-do-not-delete"
         key = "session-5/terraform.tfstate"
         region = "us-west-1"
+        dynamodb_table = "terraform-march-2022-lock"
     }
 }
 
 # Remote backend is a plugin. Plugin is a feature
+# You cannot pass or use any variables for Backend Configuration. 
+# We use Terraform Lock = DynamoDB Table
