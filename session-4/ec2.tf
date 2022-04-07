cbfe982af3ffb56fe313e44c8428159167919afd
KhuslenS <khuslen.cyber@gmail.com>
2022-04-07 16:55:07 -0500

    rolled back

--- session-4/ec2.tf
+++ session-4/ec2.tf
@@ -1,13 +1,11 @@
 resource "aws_instance" "my_ec2" {
   ami           =  data.aws_ami.amazon_linux_2.image_id
   instance_type =  var.instance_type   
-  tags = merge(
-    var.tags,
+  tags = 
     {
     Name = "${var.env}-app-instance"
     Name2 = format("%s-app-instance", var.env)
     }
-  )
   vpc_security_group_ids = [aws_security_group.my_sg.id]
 }
 
