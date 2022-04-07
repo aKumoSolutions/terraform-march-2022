c53f45c18977dda631a8243d8e0606022b0ebc8b
KhuslenS <khuslen.cyber@gmail.com>
2022-04-07 17:08:17 -0500

    updated

--- vpc/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-    backend "s3" {
-        bucket = "terraform-march-2022-backend-bucket-do-not-delete"
-        key = "vpc/terraform.tfstate"
-        region = "us-west-1"
-        dynamodb_table = "terraform-march-2022-lock"
-    }
-}
