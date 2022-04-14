2a52600b59d5627441a0b0d9b36efa0b69e383b4
KhuslenS <khuslen.cyber@gmail.com>
2022-04-13 19:20:19 -0500

    userdata.sh is updated

--- session-8/user_data.sh
+++ session-8/user_data.sh
@@ -3,3 +3,4 @@ sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo echo 'Hello World!' > /var/www/html/index.html
+sudo echo 'from EC2' >> /var/www/html/index.html
