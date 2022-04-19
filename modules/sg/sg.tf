d2de497c0fd7941e77e617391894ae8f9e02829e
KhuslenS <khuslen.cyber@gmail.com>
2022-04-18 20:37:43 -0500

    child module is updated

--- modules/sg/sg.tf
+++ modules/sg/sg.tf
@@ -5,7 +5,7 @@ resource "aws_security_group" "main" {
     #Dynamic Block was implemented Terraform Version 0.12
     dynamic "ingress" {
         for_each = var.ports
-        content  = {
+        content {
             from_port = ingress.value.from_port
             to_port = ingress.value.to_port
             cidr_blocks = ingress.value.cidr_blocks
